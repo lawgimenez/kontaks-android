d4e2d75487fd3a871b6d4996784adc4cc0ac1ead
Lawrence Gimenez <lawgimenez@gmail.com>
2016-01-10 14:57:22 +0800

    Initial intro page setup

---
+++ app/src/main/java/com/lawgimenez/kontaks/intro/KontaksIntro.java
@@ -0,0 +1,39 @@
+package com.lawgimenez.kontaks.intro;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.github.paolorotolo.appintro.AppIntro;
+
+/**
+ * Created by lawrencegimenez on 1/10/16.
+ */
+public class KontaksIntro extends AppIntro {
+
+    private static final String TAG = "KontaksIntro";
+
+    @Override
+    public void init(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public void onSkipPressed() {
+        Log.d(TAG, "onSkipPressed()");
+    }
+
+    @Override
+    public void onNextPressed() {
+        Log.d(TAG, "onNextPressed()");
+    }
+
+    @Override
+    public void onDonePressed() {
+        Log.d(TAG, "onDonePressed()");
+    }
+
+    @Override
+    public void onSlideChanged() {
+        Log.d(TAG, "onSlideChanged()");
+    }
+}
