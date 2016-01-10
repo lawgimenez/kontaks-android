49555a469ed9f238fe09cf507a78a92a94678c7b
Lawrence Gimenez <lawgimenez@gmail.com>
2016-01-10 15:20:54 +0800

    Home package

---
+++ app/src/main/java/com/lawgimenez/kontaks/home/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.lawgimenez.kontaks.home;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lawrencegimenez on 1/10/16.
+ */
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
