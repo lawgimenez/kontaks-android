0bea99dcc51a82d539ae922c1f2b984fee9252e1
Lawrence Gimenez <lawgimenez@gmail.com>
2016-01-16 14:52:34 +0800

    Added Save menu to Add Group toolbar

--- app/src/main/java/com/lawgimenez/kontaks/MainActivity.java
+++ app/src/main/java/com/lawgimenez/kontaks/MainActivity.java
@@ -3,8 +3,6 @@ package com.lawgimenez.kontaks;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.lawgimenez.kontaks.home.HomeActivity;
 import com.lawgimenez.kontaks.intro.KontaksIntro;
@@ -29,27 +27,5 @@ public class MainActivity extends AppCompatActivity {
         }
         startActivity(intent);
         finish();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/lawgimenez/kontaks/home/HomeActivity.java
+++ app/src/main/java/com/lawgimenez/kontaks/home/HomeActivity.java
@@ -8,6 +8,8 @@ import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.FrameLayout;
 
 import com.lawgimenez.kontaks.KontaksApplication;
@@ -52,6 +54,30 @@ public class HomeActivity extends AppCompatActivity {
             FragmentContactsSync fragmentContactsSync = FragmentContactsSync.newInstance();
             getSupportFragmentManager().beginTransaction().add(R.id.container_home, fragmentContactsSync).commit();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_save) {
+            Log.i(TAG, "Save Group");
+            
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void initViews() {
