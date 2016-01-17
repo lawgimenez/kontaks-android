38dfab4139c4cfa340277c48193c79a492c7526d
Lawrence Gimenez <lawgimenez@gmail.com>
2016-01-17 21:39:45 +0800

    Cursor closed

--- app/src/main/java/com/lawgimenez/kontaks/utils/KontaksDatabaseHelper.java
+++ app/src/main/java/com/lawgimenez/kontaks/utils/KontaksDatabaseHelper.java
@@ -156,6 +156,8 @@ public class KontaksDatabaseHelper extends SQLiteOpenHelper {
             } while (cursor.moveToNext());
         }
 
+        cursor.close();
+
         return listGroups;
     }
 
